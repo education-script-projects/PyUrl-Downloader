51b60c332e7425d127e9ed2e88a72d1ab0932ee8
İSMAİL TAŞDELEN <pentestdatabase@gmail.com>
2016-05-15 03:35:53 -0400

    Add files via upload

---
+++ url_downloader.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+import wget
+
+def url_downloader():
+	indirilecek_url_adresi = raw_input("İndirmek istediğiniz url adresini giriniz : ")
+	dosya_ismi = wget.download(indirilecek_url_adresi)
+
+url_downloader_ico = """
+#########################################################
+#       PYTHON - URL DOWNLOADER - GH0ST S0FTWARE        #
+######################################################### 
+#                       CONTACT                         #
+#########################################################
+#              DEVELOPER : İSMAİL TAŞDELEN              #                       
+#        Mail Address : pentestdatabase@gmail.com       #
+# LINKEDIN : https://www.linkedin.com/in/ismailtasdelen #
+#           Whatsapp : + 90 534 295 94 31               #
+#########################################################
+"""
+
+print url_downloader_ico
+
+url_downloader()
+
+
